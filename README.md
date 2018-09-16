5e731b65003639c58ff09a5a984b844a0641bb11
d2x3 <d2x3@outlook.com>
2018-09-17 05:58:54 +0800

    :beers:

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # Xdecrypt
 Xshell Xftp password decrypt
+
+```
+usage: Xdecrypt.py [-h] [-s SID] [-p PASSWORD]
+
+xsh, xfp password decrypt
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s SID, --sid SID     `username`+`sid`, user `whoami /user` in command.
+  -p PASSWORD, --password PASSWORD
+                        the password in sessions or path of sessions
+```
+
+```
+$ whoami /user
+用户信息
+----------------
+
+用户名               SID
+==================== =============================================
+computername\username sid
+
+$ python3 Xdecrypt.py
+=============C:\Users\yourname\Documents\NetSarang Computer\6\Xftp\Sessions\192.168.1.2.xfp=============
+Host:     192.168.1.2:22
+Username: root
+Password: test
+==========C:\Users\d2x3\Documents\NetSarang Computer\6\Xshell\Sessions\192.168.1.2.xsh===========
+Host:     192.168.1.2:22
+Username: root
+Password: test
+========C:\Users\d2x3\Documents\NetSarang Computer\6\Xshell\Sessions\test\192.168.1.2.xsh========
+Host:     192.168.1.2:22
+Username: root
+Password: test
+
+$ python3 Xdecrypt.py -s username+sid -p "D:\somewhere\NetSarang Computer"
+=============D:\somewhere\NetSarang Computer\6\Xftp\Sessions\192.168.1.2.xfp=============
+Host:     192.168.1.2:22
+Username: root
+Password: test
+==========D:\somewhere\NetSarang Computer\6\Xshell\Sessions\192.168.1.2.xsh===========
+Host:     192.168.1.2:22
+Username: root
+Password: test
+========D:\somewhere\NetSarang Computer\6\Xshell\Sessions\test\192.168.1.2.xsh========
+Host:     192.168.1.2:22
+Username: root
+Password: test
+
+$ python Xdecrypt.py -s username+sid -p password
+test
+```
--- Xdecrypt.py
+++ Xdecrypt.py
@@ -1,12 +1,17 @@
+import os
 import argparse
 import base64
+import configparser
+
+from win32api import GetComputerName, GetUserName
+from win32security import LookupAccountName, ConvertSidToStringSid
 from Crypto.Hash import SHA256
 from Crypto.Cipher import ARC4
 
 
 def decrypt_string(a1, a2):
     v1 = base64.b64decode(a2)
-    v3 = ARC4.new(SHA256.new(a1.encode('ascii')).digest()).decrypt(v1[:len(v1)-0x20])
+    v3 = ARC4.new(SHA256.new(a1.encode('ascii')).digest()).decrypt(v1[:len(v1) - 0x20])
     if SHA256.new(v3).digest() == v1[-32:]:
         return v3.decode('ascii')
     else:
@@ -14,10 +19,39 @@ def decrypt_string(a1, a2):
 
 
 parser = argparse.ArgumentParser(description="xsh, xfp password decrypt")
-parser.add_argument("sid", type=str, help="`username`+`sid`, use `whoami /user` in command")
-parser.add_argument("password", type=str, help="the password in sessions")
-
+parser.add_argument("-s", "--sid", default="", type=str, help="`username`+`sid`, user `whoami /user` in command.")
+parser.add_argument("-p", "--password", default="", type=str, help="the password in sessions or path of sessions")
 args = parser.parse_args()
-r = decrypt_string(args.sid, args.password)
-if r:
-    print(r)
+if not args.sid:
+    args.sid = GetUserName() + ConvertSidToStringSid(LookupAccountName(GetComputerName(), GetUserName())[0])
+if not args.password:
+    args.password = os.path.join(os.environ["USERPROFILE"], r"Documents\NetSarang Computer\6")
+
+if not os.path.isdir(args.password):
+    r = decrypt_string(args.sid, args.password)
+    if r:
+        print(r)
+
+for root, dirs, files in os.walk(args.password):
+    for f in files:
+        if f.endswith(".xsh") or f.endswith(".xfp"):
+            filepath = os.path.join(root, f)
+            cfg = configparser.ConfigParser()
+            try:
+                cfg.read(filepath)
+            except UnicodeDecodeError:
+                cfg.read(filepath, encoding="utf-16")
+
+            try:
+                if f.endswith(".xsh"):
+                    host = "{}:{}".format(cfg["CONNECTION"]["Host"], cfg["CONNECTION"]["Port"])
+                    username = cfg["CONNECTION:AUTHENTICATION"]["UserName"]
+                    password = decrypt_string(args.sid, cfg["CONNECTION:AUTHENTICATION"]["Password"])
+                else:
+                    host = "{}:{}".format(cfg["Connection"]["Host"], cfg["Connection"]["Port"])
+                    username = cfg["Connection"]["UserName"]
+                    password = decrypt_string(args.sid, cfg["Connection"]["Password"])
+                print(
+                    f"{filepath:=^100}\nHost:     {host}\nUsername: {username}\nPassword: {password}")
+            except Exception as e:
+                print(f"{filepath:=^100}\nError:{e}")
